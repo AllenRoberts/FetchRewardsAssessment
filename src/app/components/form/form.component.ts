93ff746ac25cd37b9d9fed420c522583f911ee49
AllenRoberts <coryroberts5699@gmail.com>
2022-01-26 13:06:17 -0500

    Added validation to form, styled form properly, and made page responsive

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,3 @@
-import { JsonpClientBackend } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { GetFormInfoService } from 'src/app/services/get-form-info.service';
 import { State } from '../../State';
@@ -6,7 +5,7 @@ import { State } from '../../State';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
   stateList: State[] = [];
@@ -18,38 +17,63 @@ export class FormComponent implements OnInit {
   state: string;
   occupation: string;
 
-  constructor(private getFormInfoService: GetFormInfoService) { }
-  
-  
+  constructor(private getFormInfoService: GetFormInfoService) {}
 
   ngOnInit(): void {
-    this.getFormInfoService
-    .getInfo()
-    .subscribe(
-      response=>
-      {
-        console.log(response)
-        const {occupations, states} = response
-        console.log(occupations)
-        console.log(states)
-        this.occupationList=occupations
-        this.stateList = states
-      }
-    )
-}
-
-onSubmit() {
-  if (this.password != this.rePassword) {
-    alert('Please enter the same password');
-    return;
+    this.getFormInfoService.getInfo().subscribe((response) => {
+      console.log(response);
+      const { occupations, states } = response;
+      console.log(occupations);
+      console.log(states);
+      this.occupationList = occupations;
+      this.stateList = states;
+    });
   }
-  
 
-  this.name = '';
-  this.email = '';
-  this.password = '';
-  this.rePassword = '';
-  this.occupation = 'Select Occupation';
-  this.state = 'Select State';
-}
+  validateEmail(email: string) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      return true;
+    }
+    return false;
+  }
+
+  onSubmit() {
+    if (this.name != '') {
+      alert('Please enter a name');
+    }
+    if (this.name != '') {
+      alert('Please enter a name');
+      return;
+    }
+    if (this.validateEmail(this.email)) {
+      alert('Please enter a valid email');
+      return;
+    }
+    if (this.password.length < 8) {
+      alert('Password must be 8 characters');
+      return;
+    }
+    if (this.password != this.rePassword) {
+      alert('Please enter the same password');
+      return;
+    }
+    if ((this.state = 'Select State')) {
+      alert('Please select a state');
+      return;
+    }
+    if ((this.occupation = 'Select Occupation')) {
+      alert('Please select an occupation');
+      return;
+    }
+    console.log('Point taken');
+
+    //Post Request here and move to submission page
+
+    this.name = '';
+    this.email = '';
+    this.password = '';
+    this.rePassword = '';
+    this.occupation = 'Select Occupation';
+    this.state = 'Select State';
+  }
 }
